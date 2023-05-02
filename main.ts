9ef22cc7f8afb55f21ac5db8178e83d460474bad
AGson08 <101572206+AGson08@users.noreply.github.com>
2023-05-02 13:33:14 -0400

    idk

--- main.ts
+++ main.ts
@@ -8,18 +8,19 @@ let thing: game.LedSprite = null
 let sprite: game.LedSprite = null
 let score = 0
 sprite = game.createSprite(2, 4)
+sprite.set(LedSpriteProperty.Brightness, 169)
 for (let index = 0; index < 4; index++) {
-    basic.pause(1000)
+    basic.pause(500)
     thing = game.createSprite(randint(0, 4), 0)
+    thing.set(LedSpriteProperty.Brightness, randint(69, 255))
     for (let index = 0; index < 4; index++) {
-        if (sprite.isTouching(thing)) {
-            score += 1
+        if (thing.isTouching(sprite)) {
             thing.delete()
+            score += 1
         } else {
             basic.pause(350)
             thing.change(LedSpriteProperty.Y, -1)
         }
-        thing.delete()
     }
 }
 basic.clearScreen()
